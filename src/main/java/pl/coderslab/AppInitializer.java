dd37c1e5cfc57c91dab1528532b5b5f5281a8cef
Mirosław Nowak <mnowak0995@gmail.com>
2023-12-10 12:31:20 +0100

    Problem z Tomcat

---
+++ src/main/java/pl/coderslab/AppInitializer.java
@@ -0,0 +1,32 @@
+package pl.coderslab;
+
+import jakarta.servlet.FilterRegistration;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(jakarta.servlet.ServletContext servletContext) throws ServletException {
+        //Spring ctx configuration
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.setServletContext(servletContext);
+
+        //Dispatcher servlet configuration
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+
+        //Encoding configuration
+        FilterRegistration.Dynamic fr = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        fr.setInitParameter("encoding", "UTF-8");
+        fr.setInitParameter("forceEncoding", "true");
+        fr.addMappingForUrlPatterns(null, true, "/*");
+    }
+}
